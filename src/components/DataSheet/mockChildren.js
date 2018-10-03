8a4753e68262637b6f202143d68df121ce6d987a
Onaracs <brendan.scarano@gmail.com>
2018-10-03 08:03:40 -0400

    rendering dates and kids through mapping

--- src/components/DataSheet/mockChildren.js
+++ src/components/DataSheet/mockChildren.js
@@ -3,13 +3,13 @@ export const MAGGIE = {
         name: "Maggie",
     },
     dates: {
-        "0902": { id: "0902", date: 'Sun Sep 2', hours: '0', paid: '0', notes: '' },
-        "0903": { id: "0903", date: 'Mon Sep 3', hours: '7.5', paid: '60', notes: '' },
-        "0904": { id: "0904", date: 'Tue Sep 4', hours: '6.25', paid: '50', notes: '' },
-        "0905": { id: "0905", date: 'Wed Sep 5', hours: '7.5', paid: '60', notes: '' },
-        "0906": { id: "0906", date: 'Thu Sep 6', hours: '6.25', paid: '50', notes: '' },
-        "0907": { id: "0907", date: 'Fri Sep 7', hours: '0', paid: '0', notes: '' },
-        "0908": { id: "0908", date: 'Sun Sep 8', hours: '0', paid: '0', notes: '' },
+        "090218": { id: "090218", date: 'Sun Sep 2', hours: '0', paid: '0', notes: '' },
+        "090318": { id: "090318", date: 'Mon Sep 3', hours: '7.5', paid: '60', notes: '' },
+        "090418": { id: "090418", date: 'Tue Sep 4', hours: '6.25', paid: '50', notes: '' },
+        "090518": { id: "090518", date: 'Wed Sep 5', hours: '7.5', paid: '60', notes: '' },
+        "090618": { id: "090618", date: 'Thu Sep 6', hours: '6.25', paid: '50', notes: '' },
+        "090718": { id: "090718", date: 'Fri Sep 7', hours: '0', paid: '0', notes: '' },
+        "090818": { id: "090818", date: 'Sun Sep 8', hours: '0', paid: '0', notes: '' },
     }
 }
 
@@ -18,12 +18,12 @@ export const JOHNNY = {
         name: "Johnny",
     },
     dates: {
-        "0902": { id: "0902", date: 'Sun Sep 2', hours: '0', paid: '0', notes: '' },
-        "0903": { id: "0903", date: 'Mon Sep 3', hours: '10', paid: '100', notes: '' },
-        "0904": { id: "0904", date: 'Tue Sep 4', hours: '6', paid: '60', notes: '' },
-        "0905": { id: "0905", date: 'Wed Sep 5', hours: '0', paid: '0', notes: '' },
-        "0906": { id: "0906", date: 'Thu Sep 6', hours: '6', paid: '60', notes: '' },
-        "0907": { id: "0907", date: 'Fri Sep 7', hours: '5', paid: '50', notes: '' },
-        "0908": { id: "0908", date: 'Sun Sep 8', hours: '0', paid: '0', notes: '' },
+        "090218": { id: "090218", date: 'Sun Sep 2', hours: '0', paid: '0', notes: '' },
+        "090318": { id: "090318", date: 'Mon Sep 3', hours: '10', paid: '100', notes: '' },
+        "090418": { id: "090418", date: 'Tue Sep 4', hours: '6', paid: '60', notes: '' },
+        "090518": { id: "090518", date: 'Wed Sep 5', hours: '0', paid: '0', notes: '' },
+        "090618": { id: "090618", date: 'Thu Sep 6', hours: '6', paid: '60', notes: '' },
+        "090718": { id: "090718", date: 'Fri Sep 7', hours: '5', paid: '50', notes: '' },
+        "090818": { id: "090818", date: 'Sun Sep 8', hours: '0', paid: '0', notes: '' },
     }
 }
--- src/components/DataSheet/mockData.js
+++ src/components/DataSheet/mockData.js
@@ -3,54 +3,74 @@ import { MAGGIE, JOHNNY } from './mockChildren';
 const PLACEHOLDER_SPACE = { value: ' ', readOnly: true, id: "space" };
 
 const KIDS = [MAGGIE, JOHNNY];
-console.log('KIDS', KIDS)
-// console.log('testing', ...flattenDeep(...KIDS.map(kid => HEADERS_PER_KID))
+
+const BASE_HEADERS = ['Month', 'Day', '#', 'Total', ''];
 const HEADERS_PER_KID = ['Hours per day', 'Paid/day', 'Notes', ' '];
-const HEADERS = ['Month', 'Day', '#', 'Total', '', ...flattenDeep(KIDS.map(kid => HEADERS_PER_KID))]
-    .map(text => ({ value: text, readOnly: true }));
+const HEADERS =
+    [...BASE_HEADERS, ...flattenDeep(KIDS.map(kid => HEADERS_PER_KID))].map(
+        text => ({ value: text, readOnly: true })
+    );
+
+const DATES_TO_SHOW = ["090218", "090318", "090418", "090518", "090618", "090718", "090818"];
+const DATES = {
+    "090218": { dayOfWeek: "Sun", month: "Sep", dayOfMonth: "02" },
+    "090318": { dayOfWeek: "Mon", month: "Sep", dayOfMonth: "03" },
+    "090418": { dayOfWeek: "Tue", month: "Sep", dayOfMonth: "04" },
+    "090518": { dayOfWeek: "Wed", month: "Sep", dayOfMonth: "05" },
+    "090618": { dayOfWeek: "Thu", month: "Sep", dayOfMonth: "06" },
+    "090718": { dayOfWeek: "Fri", month: "Sep", dayOfMonth: "07" },
+    "090818": { dayOfWeek: "Sat", month: "Sep", dayOfMonth: "08" },
+}
+
+const test = () => {
+    return DATES_TO_SHOW.map(dateString => {
+        console.log('dateString', dateString)
+        return [
+            { value: DATES[dateString].dayOfWeek, readOnly: true, id: "dayOfWeek" },
+            { value: DATES[dateString].month, readOnly: true, id: "month" },
+            { value: DATES[dateString].dayOfMonth, readOnly: true, id: "dayOfMonth" },
+            { value: '$0.00', readOnly: true, id: "dailyTotal" },
+            PLACEHOLDER_SPACE,
+            ...flattenDeep(KIDS.map(({ dates }) =>
+                [
+                    { value: dates[dateString].hours, id: "hours" },
+                    { value: dates[dateString].paid, id: "paid" },
+                    { value: dates[dateString].notes, id: "notes" },
+                    PLACEHOLDER_SPACE,
+                ]
+            ))
+        ]
+    })
+}
+
+console.log('test', test());
+
 
 export const mockData = [
     [
         { ...PLACEHOLDER_SPACE, colSpan: 5 },
-        ...flattenDeep(KIDS.map(({ info }) => console.log('info', info) ||
-            [
-                { value: info.name, colSpan: 3 },
-                PLACEHOLDER_SPACE,
-            ]
+        ...flattenDeep(KIDS.map(({ info }) =>
+            [{ value: info.name, colSpan: 3 }, PLACEHOLDER_SPACE]
         ))
     ],
     HEADERS,
-    [
-        { value: 'Sun', readOnly: true, id: "dayOfWeek" },
-        { value: 'Sep', readOnly: true, id: "month" },
-        { value: '2', readOnly: true, id: "dayOfMonth" },
-        { value: '$0.00', readOnly: true, id: "dailyTotal" },
-        PLACEHOLDER_SPACE,
-        ...flattenDeep(KIDS.map(({ dates }) =>
-            [
-                { value: dates["0902"].hours, id: "hours" },
-                { value: dates["0902"].paid, id: "paid" },
-                { value: dates["0902"].notes, id: "notes" },
-                PLACEHOLDER_SPACE,
-            ]
-        ))
-    ],
-    [{ value: 'Mon' }, { value: 'Sep' }, { value: '3' }],
-    [{ value: 'Tue' }, { value: 'Sep' }, { value: '4' }],
-    [{ value: 'Wed' }, { value: 'Sep' }, { value: '5' }],
-    [{ value: 'Thu' }, { value: 'Sep' }, { value: '6' }],
-    [{ value: 'Fri' }, { value: 'Sep' }, { value: '7' }],
-    [{ value: 'Sat' }, { value: 'Sep' }, { value: '8' }],
-    [{ value: '' }, { value: 'WE Sep 8 Total' }, { value: '$1000.00' }],
+    ...DATES_TO_SHOW.map(dateString => {
+        console.log('dateString', dateString)
+        return [
+            { value: DATES[dateString].dayOfWeek, readOnly: true, id: "dayOfWeek" },
+            { value: DATES[dateString].month, readOnly: true, id: "month" },
+            { value: DATES[dateString].dayOfMonth, readOnly: true, id: "dayOfMonth" },
+            { value: '$0.00', readOnly: true, id: "dailyTotal" },
+            PLACEHOLDER_SPACE,
+            ...flattenDeep(KIDS.map(({ dates }) =>
+                [
+                    { value: dates[dateString].hours, id: "hours" },
+                    { value: dates[dateString].paid, id: "paid" },
+                    { value: dates[dateString].notes, id: "notes" },
+                    PLACEHOLDER_SPACE,
+                ]
+            ))
+        ]
+    })
+    // [{ value: '' }, { value: 'WE Sep 8 Total' }, { value: '$1000.00' }],
 ]
-
-
-// const DATES = {
-//     "0902": {
-//         { value: 'Sun', "readOnly": true, "id": "dayOfWeek" },
-//         { value: 'Sep', "readOnly": true, "id": "month" },
-//         { value: '2', "readOnly": true, "id": "dayOfMonth" },
-//         { value: '$0.00', "readOnly": true, "id": "dailyTotal" },
-//         { value: ' ', "readOnly": true, "id": "space" },
-//     },
-// };
