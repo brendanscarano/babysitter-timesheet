1d86e6e6645e38351efe709f81c964f7ced49060
Kameron Robinson <kamdroidR@me.com>
2018-12-05 20:03:40 -0500

    move gql query to signup form

--- client/src/components/AuthSignupForm/index.js
+++ client/src/components/AuthSignupForm/index.js
@@ -1,22 +1,39 @@
 import React from 'react';
+import gql from 'graphql-tag';
 import { Formik } from 'formik';
 import { Mutation } from 'react-apollo';
-import { SIGN_UP_USER } from './graphql';
 import { Presentation } from './Presentation';
+
+const SIGN_UP_USER = gql`
+  mutation CreateUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    signup(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
+    }
+  }
+`;
 
 class AuthSignupForm extends React.PureComponent {
   state = {
     showPassword: false,
   }
 
-  toggleShowPassword = () =>
-    this.setState({ showPassword: !this.state.showPassword });
+  toggleShowPassword = () => this.setState({ showPassword: !this.state.showPassword });
 
   render = () => (
     <Mutation mutation={SIGN_UP_USER}>
       {(CreateUser, mutationProps) => {
-        console.log('CreateUser', CreateUser)
-        console.log('mutationProps', mutationProps)
+        console.log('CreateUser', CreateUser);
+        console.log('mutationProps', mutationProps);
         return (
           <Formik
             initialValues={{
@@ -30,23 +47,23 @@ class AuthSignupForm extends React.PureComponent {
               const errors = {};
 
               if (values.password !== values.confirmPassword) {
-                errors.confirmPassword = 'Passwords much match'
+                errors.confirmPassword = 'Passwords much match';
               }
 
               return errors;
             }}
             onSubmit={async (values, actions) => {
-              console.log('values: ', values)
-              console.log('actions: ', actions)
+              console.log('values: ', values);
+              console.log('actions: ', actions);
               const response = await CreateUser({
                 variables: {
                   firstName: values.firstName,
                   lastName: values.lastName,
                   email: values.email,
                   password: values.password,
-                }
-              })
-              console.log('response', response)
+                },
+              });
+              console.log('response', response);
               // On success redirect to main page
             }}
           >
@@ -61,7 +78,7 @@ class AuthSignupForm extends React.PureComponent {
               />
             )}
           </Formik>
-        )
+        );
       }}
     </Mutation>
   )
