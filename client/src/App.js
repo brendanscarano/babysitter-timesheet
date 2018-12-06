a540fe8e730fab97243a76bbd46e2f5a4629c3d7
Kameron Robinson <kamdroidR@me.com>
2018-12-05 20:05:57 -0500

    use HOC add Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
@@ -12,6 +8,8 @@ import NewChild from './screens/NewChild';
 import LoginSignup from './screens/LoginSignup';
 import ChildInfo from './screens/ChildInfo';
 import Main from './screens/Main';
+import { Layout } from './components/Layout';
+import { RequireSubscription } from './hocs/RequireSubscription';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
@@ -21,13 +19,24 @@ const App = () => (
   <ApolloProvider client={client}>
     <Router>
       <Switch>
-        {/* Redirect if no user logged in */}
-        {/* <Redirect from="/" to="/Oct-2018" /> */}
-        <Route exact path="/child/:id" component={ChildInfo} />
-        <Route exact path="/new-child" component={NewChild} />
-        <Route exact path="/my-profile" component={MyProfile} />
-        <Route exact path="/:date" component={Main} />
-        <Route exact path="/" component={LoginSignup} />
+        <Layout isUserSignedIn>
+          {/** Add user state here  */}
+          <Route exact path="/register" component={LoginSignup} />
+          <Route exact path="/welcome" render={() => <div>Hello world</div>} />
+          <Route
+            path="/"
+            component={RequireSubscription(() => (
+              <Switch>
+                {/* Redirect if no user logged in */}
+                {/* <Redirect from="/" to="/Oct-2018" /> */}
+                <Route exact path="/child/:id" component={ChildInfo} />
+                <Route exact path="/new-child" component={NewChild} />
+                <Route exact path="/my-profile" component={MyProfile} />
+                <Route exact path="/:date" component={Main} />
+              </Switch>
+            ))}
+          />
+        </Layout>
       </Switch>
     </Router>
   </ApolloProvider>
