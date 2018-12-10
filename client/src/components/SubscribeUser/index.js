d17c372c56b3b416df79520b2ff287cd176ac3f6
Kameron Robinson <kamdroidR@me.com>
2018-12-09 23:22:33 -0500

    lint

--- client/src/components/SubscribeUser/index.js
+++ client/src/components/SubscribeUser/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { gql } from 'apollo-boost';
 import { Mutation } from 'react-apollo';
 import StripeCheckout from 'react-stripe-checkout';
+import { ME_QUERY } from '../../graphql/queries/ME_QUERY';
 
 const CREATE_SUBSCRIPTION_MUTATION = gql`
   mutation CreateSubscriptionMutation($source: String!, $ccLast4: String!) {
@@ -13,14 +14,12 @@ const CREATE_SUBSCRIPTION_MUTATION = gql`
 `;
 
 export default () => (
-  <Mutation mutation={CREATE_SUBSCRIPTION_MUTATION}>
+  <Mutation mutation={CREATE_SUBSCRIPTION_MUTATION} refetchQueries={ME_QUERY} awaitRefetchQueries>
     {mutate => (
       <StripeCheckout
-        token={async (token) => {
-          const response = await mutate({
-            variables: { source: token.id, ccLast4: token.card.last4 },
-          });
-        }}
+        token={async token => mutate({
+          variables: { source: token.id, ccLast4: token.card.last4 },
+        })}
         stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}
       />
     )}
