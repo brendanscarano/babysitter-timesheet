c50bd81f25622f9ea70ed290f408f84c82548779
Kameron Robinson <kamdroidR@me.com>
2018-12-11 21:42:43 -0500

    init redis

--- server/src/redis.js
+++ server/src/redis.js
@@ -3,6 +3,10 @@ const Redis = require("ioredis");
 const redis =
   process.env.NODE_ENV === "production"
     ? new Redis(process.env.REDIS_URL)
-    : new Redis();
+    : new Redis({
+      port: 6379,
+      host: '127.0.0.1',
+      db: 0
+    });
 
 module.exports = { redis }
