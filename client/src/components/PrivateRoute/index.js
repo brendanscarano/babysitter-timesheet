087819709f1911a4650951a9aeb3c527623f48d8
Kameron Robinson <kamdroidR@me.com>
2018-12-11 22:46:03 -0500

    sid

--- client/src/components/PrivateRoute/index.js
+++ client/src/components/PrivateRoute/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.setItem('sid');
 
   return (
     <Route
