08a7e2a7f1c51e2ccefe405e7234da56b987a94b
Kameron Robinson <kamdroidR@me.com>
2018-12-13 02:40:12 -0500

    update auth

--- server/src/resolvers/mutations/auth.js
+++ server/src/resolvers/mutations/auth.js
@@ -5,35 +5,59 @@ const { APP_SECRET } = require('../../utils')
 module.exports = {
     signup: async (parent, { firstName, lastName, email, password }, ctx) => {
         const hashedPassword = await hash(password, 10);
-
+    
         try {
-            const user = await ctx.prisma.createUser({
-                firstName,
-                lastName,
-                email: email.toLowerCase(),
-                password: hashedPassword,
-            })
-
-            return {
-                token: sign({ userId: user.id }, APP_SECRET),
-                user,
-            }
+          const user = await ctx.prisma.createUser({
+            firstName,
+            lastName,
+            email: email.toLowerCase(),
+            password: hashedPassword,
+          })
+    
+          const { sessionID } = ctx.req;
+    
+          ctx.session.userId = user.id;
+          if (sessionID) {
+            await ctx.redis.lpush(`${userSessionIdPrefix}${user.id}`, sessionID);
+          }
+    
+          return { token: sessionID, user };
         } catch (err) {
-            throw new Error(err.message);
+          throw new Error(err.message);
         }
-    },
-    login: async (parent, { email, password }, ctx) => {
+      },
+      login: async (parent, { email, password }, ctx) => {
         const user = await ctx.prisma.user({ email: email.toLowerCase() })
         if (!user) {
-            throw new Error(`No user found for email: ${email}`)
+          throw new Error(`No user found for email: ${email}`)
         }
         const passwordValid = await compare(password, user.password)
         if (!passwordValid) {
-            throw new Error('Invalid password')
+          throw new Error('Invalid password')
         }
-        return {
-            token: sign({ userId: user.id }, APP_SECRET),
-            user,
+    
+        const { sessionID } = ctx.req;
+        
+        ctx.session.userId = user.id;
+        if (sessionID) {
+          await ctx.redis.lpush(`${userSessionIdPrefix}${user.id}`, sessionID);
         }
-    },
+    
+        return { token: sessionID, user };
+      },
+      logout: async (_, __, { session, redis, res }) => {
+        const { userId } = session;
+        if (userId) {
+          removeAllUsersSessions(userId, redis);
+          session.destroy(err => {
+            if (err) {
+              console.log(err);
+            }
+          });
+          res.clearCookie("qid");
+          return true;
+        }
+    
+        return false;
+      },
 }
