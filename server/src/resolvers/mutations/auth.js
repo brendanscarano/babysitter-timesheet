46019c9c1cf1d212ca5ce918c414b97fc91c6c49
Kameron Robinson <KamdroidR@me.com>
2018-12-15 11:58:58 -0500

    import prefix (#12)

--- server/src/resolvers/mutations/auth.js
+++ server/src/resolvers/mutations/auth.js
@@ -1,63 +1,62 @@
 const { hash, compare } = require('bcrypt')
-const { sign } = require('jsonwebtoken')
-const { APP_SECRET } = require('../../utils')
+const { userSessionIdPrefix } = require('../../constants');
 
 module.exports = {
-    signup: async (parent, { firstName, lastName, email, password }, ctx) => {
-        const hashedPassword = await hash(password, 10);
-    
-        try {
-          const user = await ctx.prisma.createUser({
-            firstName,
-            lastName,
-            email: email.toLowerCase(),
-            password: hashedPassword,
-          })
-    
-          const { sessionID } = ctx.req;
-    
-          ctx.session.userId = user.id;
-          if (sessionID) {
-            await ctx.redis.lpush(`${userSessionIdPrefix}${user.id}`, sessionID);
-          }
-    
-          return { token: sessionID, user };
-        } catch (err) {
-          throw new Error(err.message);
+  signup: async (parent, { firstName, lastName, email, password }, ctx) => {
+    const hashedPassword = await hash(password, 10);
+
+    try {
+      const user = await ctx.prisma.createUser({
+        firstName,
+        lastName,
+        email: email.toLowerCase(),
+        password: hashedPassword,
+      })
+
+      const { sessionID } = ctx.req;
+
+      ctx.session.userId = user.id;
+      if (sessionID) {
+        await ctx.redis.lpush(`${userSessionIdPrefix}${user.id}`, sessionID);
+      }
+
+      return { token: sessionID, user };
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  },
+  login: async (parent, { email, password }, ctx) => {
+    const user = await ctx.prisma.user({ email: email.toLowerCase() })
+    if (!user) {
+      throw new Error(`No user found for email: ${email}`)
+    }
+    const passwordValid = await compare(password, user.password)
+    if (!passwordValid) {
+      throw new Error('Invalid password')
+    }
+
+    const { sessionID } = ctx.req;
+
+    ctx.session.userId = user.id;
+    if (sessionID) {
+      await ctx.redis.lpush(`${userSessionIdPrefix}${user.id}`, sessionID);
+    }
+
+    return { token: sessionID, user };
+  },
+  logout: async (_, __, { session, redis, res }) => {
+    const { userId } = session;
+    if (userId) {
+      removeAllUsersSessions(userId, redis);
+      session.destroy(err => {
+        if (err) {
+          console.log(err);
         }
-      },
-      login: async (parent, { email, password }, ctx) => {
-        const user = await ctx.prisma.user({ email: email.toLowerCase() })
-        if (!user) {
-          throw new Error(`No user found for email: ${email}`)
-        }
-        const passwordValid = await compare(password, user.password)
-        if (!passwordValid) {
-          throw new Error('Invalid password')
-        }
-    
-        const { sessionID } = ctx.req;
-        
-        ctx.session.userId = user.id;
-        if (sessionID) {
-          await ctx.redis.lpush(`${userSessionIdPrefix}${user.id}`, sessionID);
-        }
-    
-        return { token: sessionID, user };
-      },
-      logout: async (_, __, { session, redis, res }) => {
-        const { userId } = session;
-        if (userId) {
-          removeAllUsersSessions(userId, redis);
-          session.destroy(err => {
-            if (err) {
-              console.log(err);
-            }
-          });
-          res.clearCookie("qid");
-          return true;
-        }
-    
-        return false;
-      },
+      });
+      res.clearCookie("qid");
+      return true;
+    }
+
+    return false;
+  },
 }
