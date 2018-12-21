83b91d51d60dde9a5624861c1064858a9fc5ac5b
Kameron Robinson <kamdroidR@me.com>
2018-12-21 18:12:05 -0500

    add build date to client

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,6 +1,9 @@
-#! ~/bin/bash
+#!/bin/bash
 
+datetime=$(date "+%Y-%m-%d %H:%M:%S")
+export REACT_APP_BUILD_DATE=$datetime
 yarn build
+unset REACT_APP_BUILD_DATE
 tar -cvf ./deploy.tar --exclude='*.map' ./captain-definition ./build/*
 captainduckduck deploy -t ./deploy.tar
 rm -rf ./deploy.tar
--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+window.buildDate = process.env.REACT_APP_BUILD_DATE;
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
